de08e4b94eaa665209e371b3c22ae8c1f0860504
juergen <juergen>
2007-05-25 16:53:04 +0000

    Added version comment for Tom Tromeys package.el Added project url

--- lua-mode.el
+++ lua-mode.el
@@ -10,7 +10,8 @@
 ;;              with tons of assistance from
 ;;              Paul Du Bois <pld-lua@gelatinous.com> and
 ;;              Aaron Smith <aaron-lua@gelatinous.com>.
-
+;; URL:		http://lua-mode.luaforge.net/
+;; Version:	20070525
 ;; This file is NOT part of Emacs.
 ;;
 ;; This program is free software; you can redistribute it and/or
