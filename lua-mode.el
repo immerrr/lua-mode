aef55a6c97e5c894a87be8c9b2fc2a3f8bf24d03
immerrr <immerrr@gmail.com>
2011-03-15 22:15:05 +0300

    Update contact info in README; bump version

--- lua-mode.el
+++ lua-mode.el
@@ -12,7 +12,7 @@
 ;;              Paul Du Bois <pld-lua@gelatinous.com> and
 ;;              Aaron Smith <aaron-lua@gelatinous.com>.
 ;; URL:		http://lua-mode.luaforge.net/
-;; Version:	20110121
+;; Version:	20110315
 ;; This file is NOT part of Emacs.
 ;;
 ;; This program is free software; you can redistribute it and/or
