89b1a6d16166a0ad3c66d97e8e7c6e851c73e1cd
Gallagher Pryor <gallagher.pryor@gmail.com>
2015-07-25 05:12:44 -0500

    save buffer before sending to lua. now operable.

--- lua-mode.el
+++ lua-mode.el
@@ -1816,7 +1816,7 @@ issues with process-send-string and large regions."
   (interactive)
   (let* ((lua-file (or (buffer-file-name) (buffer-name)))
          (command (format "print(''); dofile('%s');" lua-file)))
-    (lua-send-buffer)
+    (save-buffer)
     (lua-send-string command)))
 
 (defun lua-restart-with-whole-file ()
