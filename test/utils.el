674a98659f85f39c97c8f38269230cc84bda02c9
immerrr <immerrr+lua@gmail.com>
2019-05-13 07:44:49 +0200

    Add a comment regarding use of local process variables in tests

--- test/utils.el
+++ test/utils.el
@@ -126,6 +126,13 @@ This is a mere typing/reading aid for lua-mode's font-lock tests."
   (declare (debug (&rest form)))
   `(with-temp-buffer
      (lua-mode)
+     ;; lua-process/-buffer variables are not buffer-local in lua-mode by
+     ;; default, so it might seem an unnecessary nuisance, but by making them
+     ;; local in tests we:
+     ;; - add another layer of encapsulation preventing process-related
+     ;;   variables from leaking between tests
+     ;; - ensure everything works even if the user detaches one of the buffers
+     ;;   to have a separate inferior process
      (set (make-local-variable 'lua-process) nil)
      (set (make-local-variable 'lua-process-buffer) nil)
      (font-lock-fontify-buffer)
