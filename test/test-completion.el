a959556ca24a561dd4b89fd987c6104af54dff42
JD Smith <jdsmith@rain.local>
2019-05-16 16:09:51 -0400

    Start new process with run-lua

--- test/test-completion.el
+++ test/test-completion.el
@@ -8,67 +8,59 @@
   (it "completes top-level globals"
     (with-lua-buffer
      (insert "tabl")
-     (lua-get-create-process)
+     (run-lua)
      (completion-at-point)
      (expect (buffer-string) :to-equal "table")))
   (it "completes nested globals"
     (with-lua-buffer
      (insert "table.ins")
-     (lua-get-create-process)
+     (run-lua)
      (completion-at-point)
      (expect (buffer-string) :to-equal "table.insert")))
   (it "completes nested globals with spaces"
     (with-lua-buffer
      (insert "table. ins")
-     (lua-get-create-process)
+     (run-lua)
      (completion-at-point)
      (expect (buffer-string) :to-equal "table. insert")))
-    (it "completes nested globals inside function call"
+  (it "completes nested globals inside function call"
     (with-lua-buffer
      (insert "print(table.con)")
      (backward-char)
-     (lua-get-create-process)
+     (run-lua)
      (completion-at-point)
      (expect (buffer-string) :to-equal "print(table.concat)")))
   (it "completes nested globals with newline"
     (with-lua-buffer
      (insert "table.\n  ins")
-     (lua-get-create-process)
+     (run-lua)
      (completion-at-point)
      (expect (buffer-string) :to-equal "table.\n  insert"))))
 
 (describe "Test lua-complete-function with lua-local-require-regexp"
-  :var(libs locals)
+  :var(libs locals comp-lib comp-lib-value comp-lib-mult)
 
   (before-all
-   (with-lua-buffer-no-kill
+   (with-lua-buffer
     (insert "local xyz = require('file')\n")
-    (let ((lua-local-require-completions t))
-      (setq this-lua-buffer (current-buffer)
-	    libs (lua-local-libs)
-	    locals (lua-top-level-locals (mapcar 'car libs))))))
-
-  (after-all
-   (with-current-buffer this-lua-buffer
-     (when (buffer-live-p lua-process-buffer)
-       (lua-kill-process))))
-
-  (it "initializes libs and locals correctly"
-      (with-current-buffer this-lua-buffer
-	(expect libs :to-equal '(("xyz" "'file'")))
-	(expect locals :to-be nil)))
+    (setq lua-local-require-completions t
+	  libs (lua-local-libs)
+	  locals (lua-top-level-locals (mapcar 'car libs)))
+    (run-lua)
+    (with-current-buffer lua-process-buffer
+      (setq comp-lib (lua--get-completions "xy" libs locals)
+	    comp-lib-value (lua--get-completions "xyz.ab" libs locals)
+	    comp-lib-mult (sort (lua--get-completions "xyz." libs locals) #'string-lessp)))))
 
   (it "completes locally-required libraries"
-      (with-current-buffer this-lua-buffer
-	(expect (lua--get-completions "xy" libs locals)
-		:to-equal '("xyz"))))
+      (expect comp-lib :to-equal '("xyz")))
+
+  (it "initializes libs and locals correctly"
+      (expect libs :to-equal '(("xyz" "'file'")))
+      (expect locals :to-be nil))
       
   (it "completes single value from locally-required libraries"
-      (with-current-buffer this-lua-buffer
-	(expect (lua--get-completions "xyz.ab" libs locals)
-		:to-equal '("abc"))))
+      (expect comp-lib-value :to-equal '("xyz.abc")))
       
   (it "completes multiple values from locally-required libraries"
-      (with-current-buffer this-lua-buffer
-	(expect (sort (lua--get-completions "xyz." libs locals) #'string-lessp)
-		:to-equal '("abc" "def")))))
+      (expect comp-lib-mult :to-equal '("xyz.abc" "xyz.def"))))
