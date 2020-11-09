1135932247cc71b90660d51fd494175f73ddbc17
Nikita Bloshchanevich <nikblos@outlook.com>
2020-11-09 11:43:11 +0100

    test-process: check if the buffer switching works

--- test/test-process.el
+++ test/test-process.el
@@ -9,13 +9,16 @@
 (describe "`lua-start-process'"
   (it "doesn't hang for an already-running process"
     ;; Acquire a *lua* repl buffer
-    (lua-start-process)
-    ;; Input some text
-    (with-current-buffer lua-process-buffer
-      (insert "table.insert"))
-    ;; Try restarting the repl buffer
-    (lua-start-process)
-    ;; `lua-start-process' shouldn't hang
-    (expect t)))
+    (save-current-buffer
+      (funcall-interactively #'lua-start-process)
+      ;; Input some text
+      (with-current-buffer lua-process-buffer
+        (insert "table.insert"))
+      (switch-to-buffer (get-buffer-create "*scratch*"))
+      ;; Try restarting the repl buffer
+      (funcall-interactively #'lua-start-process)
+
+      ;; `lua-start-process' shouldn't hang, and it should have switched back.
+      (expect (current-buffer) :to-be lua-process-buffer))))
 
 ;;; test-process.el ends here
